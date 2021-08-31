9967484ded462670f67d8393566a559c62f71fe1
MaryamPayenda <maryampayanda12@gmail.com>
2021-08-31 14:05:07 +0200

    get one

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,13 +1,36 @@
 const express = require("express");
 const usersData = require("../model/usersModel");
-// middleware
-// adding new user to the collection
 
-const getAllUsers = async (req, res) => {
-  let allUsers = await usersData.find();
-  res.json(allUsers);
+// ------ middlewares-------------------
+// middleware to find one by name
+const getUserByName = async (req, res, next) => {
+  let userByName;
+  try {
+    userByName = await usersData.findOne({
+      userName: req.params.userName,
+    });
+
+    if (userByName == null) {
+      return res.status(404).json({ message: "User Not found" });
+    }
+  } catch (err) {
+    res.status(err.status).json({ message: err.message });
+  }
+  res.users = userByName;
+  next();
 };
 
+//  to get all users
+const getAllUsers = async (req, res) => {
+  try {
+    let allUsers = await usersData.find();
+    res.status(200).json(allUsers);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// add new user
 const addNewUser = async (req, res) => {
   const user = new usersData({
     userName: req.body.userName,
@@ -25,42 +48,47 @@ const addNewUser = async (req, res) => {
   }
 };
 
+//delete a user by id
 const deleteUser = async (req, res) => {
   const id = req.params.id;
   await usersData.findByIdAndDelete(id);
   res.send("User deleted successfully!");
 };
 
-// to get all documents
-// const getAllUsers = async (req, res) => {
-//   console.log("hello");
-//   try {
-//     const users = await usersData.find();
-//     res.status(200).json(
-//       users.map((user) => {
-//         return {
-//           userId: user._id,
-//           userName: user.userName,
-//           userPass: user.userPass,
-//           age: user.age,
-//           fwd: user.fwd,
-//           toolStack: user.toolStack,
-//           email: user.email,
-//           userAddedDate: user.userAddedDate,
-//           request: {
-//             type: "GET",
-//             url: `http://localhost:5000/users/${user.userName}`,
-//           },
-//         };
-//       })
-//     );
-//   } catch (err) {
-//     res.status(500).json({ message: err.message });
-//   }
-// };
+// Get one by name  http://localhost:5001/users/Maryam
+const getOne = async (req, res) => {
+  try {
+    res.status(200).json(res.users);
+  } catch (err) {
+    res.status(err.status).json({ message: err.message });
+  }
+};
 
+// update user
+const updateUser = async (res, req) => {};
+
+// patch one one info in table in put will update the whole data in the table
+// const updateUserInfo =
+//   (getUserByID,
+//   async (req, res) => {
+//     try {
+//       const userByID = await usersData.findByIdAndUpdate(req.params.userName, {
+//         userName: req.body.userName || res.users,
+//         userPass: req.body.userPass || res.users,
+//         age: req.body.name || res.users,
+//         fwd: req.bpdy.name || res.users,
+//         toolStack: req.body.toolStack || res.users,
+//         email: req.body.email || res.users,
+//       });
+//     } catch (err) {
+//       res.status(err.status).json({ message: err.message });
+//     }
+//   });
 module.exports = {
   getAllUsers,
   addNewUser,
   deleteUser,
+  // updateUserInfo,
+  getOne,
+  getUserByName,
 };
--- router/users.js
+++ router/users.js
@@ -4,11 +4,16 @@ const UsersData = require("../model/usersModel");
 const {
   getAllUsers,
   addNewUser,
-  deleteUser,
+  getUserByName,
+
+  // deleteUser,
+  getOne,
+  updateUserInfo,
 } = require("../controllers/usersControllers");
 
 router.route("/").post(addNewUser);
 router.route("/").get(getAllUsers);
-router.route("/:id").delete(deleteUser);
+// router.route("/:id").delete(deleteUser);
+router.route("/:userName").get(getUserByName, getOne);
 
 module.exports = router;
