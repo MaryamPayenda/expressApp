ae1f965075386a2c992c33a54910a4a1e117bfe9
MaryamPayenda <maryampayanda12@gmail.com>
2021-09-13 09:20:09 +0200

    added

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 server.listen(PORT, () => {
-  console.log(`server is listening on port ${PORT}`);
+  console.log(`Server is listening on http://localhost:${PORT}`);
 });
